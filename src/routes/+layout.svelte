8aafaa516fb2d602e5ee4c4e3d69bcaff3e4784e
Reza Ali Nirwansyah <rezaalinirwansyah@gmail.com>
2026-04-25 21:33:55 +0700

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<div class="flex min-h-screen flex-col bg-radial from-gray-100 via-white to-cyan-50 text-slate-800">
+	<div class="mx-auto flex min-h-screen w-full max-w-3xl flex-col px-4">
+		{@render children()}
+	</div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<div class="flex min-h-screen flex-col items-center justify-center">
+	<div class="flex flex-col items-center gap-6">
+		<h1 class="text-5xl font-bold text-black">Tebak Angka</h1>
+		<p class="text-gray-700">Masukkan Angka 1-100</p>
+	</div>
+
+	<div class="mt-8 flex flex-col items-center gap-4">
+		<input
+			type="text"
+			placeholder="Tebak Angka..."
+			class="focus:outline-nonet w-full border-b border-gray-300 bg-transparent text-center text-slate-800 placeholder:text-gray-400 focus:border-cyan-400"
+		/>
+		<button class=" w-full rounded bg-blue-500 px-4 py-2 text-white">Submit</button>
+	</div>
+</div>
